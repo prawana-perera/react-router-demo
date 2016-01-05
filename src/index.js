577a928dfc2b8d274b9a7030cfed8be1b6aa89a7
Prawana Perera <prawana.perera@gmail.com>
2016-01-05 22:01:00 +1100

    added material-ui scafolding

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router'
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import Main from './components/Main';
 import About from './components/About';
 import Contact from './components/Contact';
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 ReactDOM.render((
   <Router>
